d3c64b80034d37e4056b80aaf56e2aeec8d34c73
Brian Perry <contact.brianperry@gmail.com>
2021-07-25 16:01:10 -0500

    3224529: Upgrade Lit Dependency to Lit 2.0

--- gdwc-menu/gdwc-menu.js
+++ gdwc-menu/gdwc-menu.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css } from 'lit';
 import { denormalize } from 'linkset-menu';
 
 export class GdwcMenu extends LitElement {
