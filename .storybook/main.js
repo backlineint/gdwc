6d2d308c06dd2b0369e94a5b1da6a485341aeea8
brianperry <backlineint@2304500.no-reply.drupal.org>
2022-07-22 13:17:36 +0000

    Issue #3273194 by finnsky, brianperry, andypost: NPM Audit

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,5 +5,8 @@ module.exports = {
     '../**/gdwc*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  core: { builder: '@storybook/builder-vite' },
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  framework: '@storybook/web-components',
 };
