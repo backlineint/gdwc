908014fba0e932f6122e929c9df7b1e0d19595d3
Brian Perry <contact.brianperry@gmail.com>
2022-07-22 19:13:36 -0400

    Attempt to fix crossorigin path issue via vite config

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,4 +9,11 @@ module.exports = {
     builder: '@storybook/builder-vite',
   },
   framework: '@storybook/web-components',
+  // Fix for iFrame path issues from https://github.com/storybookjs/storybook/discussions/17433
+  async viteFinal(config) {
+    config.base = process.env.BASE_URL || config.base;
+
+    // return the customized config
+    return config;
+  },
 };
